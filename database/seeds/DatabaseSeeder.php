0cac06fb3836e3b1e136e80a5efb6ae9145fa740
Shellrean <wandinak17@gmail.com>
2020-07-05 22:20:59 +0700

    seeder(add): Add jurusans seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,5 +14,6 @@ class DatabaseSeeder extends Seeder
         $this->call(UsersTableSeeder::class);
         $this->call(AgamaSeeder::class);
         $this->call(PermissionTable::class);
+        $this->call(JurusanSeeder::class);
     }
 }
